3fdf46dd5fabbbd82c9b95e6b899af594d84446d
Griffin Dougherty <griffind92@gmail.com>
2016-06-16 22:16:46 -0400

    Final commit

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 $(document).ready(function(){
 	$("body").append("<div id=\"wrapper\"></div>");
+	
+	draw(16);	
+	
+	$("button").click(function(){
+		$(".inner").removeClass("fill");
+		var size = prompt("New Dimensions: ");
+		dim = +size;
+		$("table").remove();
+		draw(dim);
+	});	
+});
+
+function draw(dim){
+	
+	var size = Math.floor(800/dim);
+	
 	$("#wrapper").append("<table></table>");
-	for(var i=1; i<=16; i++){
+	for(var i=1; i<=dim; i++){
 		$("table").append("<tr id="+"\""+i+"\""+"></tr>");
-		for(var j=1; j<=16; j++){
+		for(var j=1; j<=dim; j++){
 			$("#"+i).append("<td></td>");
 		}
-	}
-	$("td").append("<div></div>");
-	$("div").mouseenter(function(){
+	}	
+	$("td").append("<div class=\"inner\"></div>");
+	$(".inner").width(size).height(size);
+	$(".inner").mouseenter(function(){
 		$(this).addClass("fill");
 	});
-	$("button").click(function(){
-		$("div").removeClass("fill");
-	});
-});
+}
+
